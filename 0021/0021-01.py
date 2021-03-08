67c45347c5d0a4fb8fa55349dc612fab3127e528
CassHsu <cass.hsu@gmail.com>
2021-03-08 10:31:12 +0800

    Create 0021-01.py

---
+++ 0021/0021-01.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        curr = ListNode(None)
+        head = curr
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+            
+        if l1:
+            curr.next = l1
+        if l2:
+            curr.next = l2
+        
+        return head.next
