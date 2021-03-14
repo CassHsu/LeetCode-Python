8b73949ce601a2006b39e47319f73afa89b71de0
CassHsu <cass.hsu@gmail.com>
2021-03-14 12:22:53 +0800

    Approach 1: Recursion

---
+++ 0700/0700-01.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root == None:
+            return None
+        if root.val == val:
+            return root
+        
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        return self.searchBST(root.right, val)
