37fc355625b5acbc2cdd305eba702a1f0238f4d5
CassHsu <cass.hsu@gmail.com>
2021-05-03 18:17:35 +0800

    Create 1848-01.py

---
+++ 1848/1848-01.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        ans = inf
+        for i, n in enumerate(nums):
+            if n == target:
+                ans = min(ans, abs(i - start))
+        return ans
