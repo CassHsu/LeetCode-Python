6b222310181a8b6c0075872d5b1f412f6b754023
CassHsu <cass.hsu@gmail.com>
2021-06-03 20:58:09 +0800

    Approach 2: Mathematical Digital Root

---
+++ 0258/0258-02.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        m9 = num % 9
+        if m9 == 0:
+            return 9
+        return m9
