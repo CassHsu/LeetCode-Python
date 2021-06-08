77a1fcb4d1119fabd35f64e03f9b2517c240c28d
CassHsu <cass.hsu@gmail.com>
2021-06-08 21:51:00 +0800

    Create 0189-01.py

---
+++ 0189/0189-01.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        size = len(nums)
+        k = k % size;
+        
+        nums[:] = nums[size - k:] + nums[:size - k]
