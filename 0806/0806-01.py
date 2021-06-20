0ea14c597fab56d7fb4a628b750d6f5fa8bc2db3
CassHsu <cass.hsu@gmail.com>
2021-06-20 21:51:38 +0800

    Create 0806-01.py

---
+++ 0806/0806-01.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfLines(self, widths: List[int], s: str) -> List[int]:
+        line, width = 1, 0
+        
+        for c in s:
+            w = widths[ord(c) - ord('a')]
+            width += w
+            
+            if width > 100:
+                line += 1
+                width = w
+        
+        return [line, width]
