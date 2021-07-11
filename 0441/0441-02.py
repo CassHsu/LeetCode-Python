15729ed93ebd595b24c44715ab7581761dc12ce2
CassHsu <cass.hsu@gmail.com>
2021-07-11 22:56:38 +0800

    Create 0441-02.py

---
+++ 0441/0441-02.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        k = 1
+        
+        while n >= k:
+            n -= k
+            k += 1
+            
+        return k - 1
