5d22e196de4b5804badfed15e5310aec6be58745
CassHsu <cass.hsu@gmail.com>
2021-08-26 18:48:43 +0800

    Create 0013-01.py

---
+++ 0013/0013-01.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        ans = 0
+        
+        m = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+        
+        for i in range(len(s) - 1):
+            if m[s[i]] < m[s[i + 1]]:
+                ans -= m[s[i]]
+            else:
+                ans += m[s[i]]
+        
+        ans += m[s[-1]]
+        return ans
