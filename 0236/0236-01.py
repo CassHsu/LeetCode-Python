69ae83a29172faebf63024c9685ff4103ed98c94
CassHsu <cass.hsu@gmail.com>
2021-09-14 09:34:07 +0800

    Recursion

---
+++ 0236/0236-01.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        
+        if root == p or root == q:
+            return root
+        
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+        
+        if l and r:
+            return root
+        
+        if not l and not r:
+            return None
+        
+        return r if not l else l
