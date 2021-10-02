0f90422b6a73d428bd4dfc4133b9a9e88e5244e0
CassHsu <cass.hsu@gmail.com>
2021-10-02 12:19:01 +0800

    Update 0283-01.py

--- 0283/0283-01.py
+++ 0283/0283-01.py
@@ -1,8 +1,12 @@
 class Solution:
-    def moveZeroes(self, nums) -> None:
-        p = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                if i != p:
-                    nums[p], nums[i] = nums[i], nums[p]
-                p += 1
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0: 
+                if fast != 0:
+                    nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
