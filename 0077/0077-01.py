73a31cd1d3e8cb6d0d63acb23809cbad71d68df2
CassHsu <cass.hsu@gmail.com>
2021-10-10 07:54:49 +0800

    Update 0077-01.py

--- 0077/0077-01.py
+++ 0077/0077-01.py
@@ -2,14 +2,16 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         ans = []
         
-        def backtrack(curr, idx):    
+        def backtrack(curr, m):
+            nonlocal n, k
             if len(curr) == k:
                 ans.append(list(curr))
-                
-            for i in range(idx, n + 1):
-                curr.append(i)
-                backtrack(curr, i + 1)
-                curr.pop()
+                return
             
+            for i in range(m, n+1):
+                curr.append(i)
+                backtrack(curr, i+1)
+                curr.pop()
+        
         backtrack([], 1)
         return ans
