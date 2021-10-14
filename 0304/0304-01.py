dee206ca26c1c9a613e0ebd8a566d2b40f0ea434
CassHsu <cass.hsu@gmail.com>
2021-10-14 09:24:45 +0800

    DP

---
+++ 0304/0304-01.py
@@ -0,0 +1,17 @@
+class NumMatrix:
+    dp = None
+    def __init__(self, matrix: List[List[int]]):
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        if m == 0 or n == 0:
+            return
+        
+        self.dp = [[0] * (n+1) for _ in range(m + 1)]
+        for r in range(m):
+            for c in range(n):
+                self.dp[r + 1][c + 1] = self.dp[r + 1][c] + self.dp[r][c + 1] + matrix[r][c] - self.dp[r][c];
+        
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.dp[row2 + 1][col2 + 1] - self.dp[row1][col2 + 1] - self.dp[row2 + 1][col1] + self.dp[row1][col1];
