d95b626d9b2d07342262e9cf248799d56d8114c7
CassHsu <cass.hsu@gmail.com>
2021-10-18 09:44:37 +0800

    DP - Bottom up

---
+++ 0392/0392-02.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+        
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(len(t) - 1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i+1][j+1] + 1
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        
+        return dp[0][0] == len(s)
