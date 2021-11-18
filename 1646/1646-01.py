ae7261234a3fd670cca8060693d01d32907fdaa3
CassHsu <cass.hsu@gmail.com>
2021-11-18 08:48:45 +0800

    DP

--- 1646/1646-01.py
+++ 1646/1646-01.py
@@ -3,15 +3,11 @@ class Solution:
         if n == 0:
             return 0
         
-        dp = [0 for i in range(n + 1)]
+        dp = [0] * (n+1)
         dp[1] = 1
         
-        m = n // 2
-        if n % 2 != 0:
-            m += 1
-
-        for i in range(1, m):
+        for i in range(1, (n+1) // 2):
             dp[i * 2] = dp[i]
             dp[(i * 2) + 1] = dp[i] + dp[i + 1]
-        
+            
         return max(dp)
