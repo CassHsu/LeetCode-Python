8967d7f6c573d52e69bdb690e6546643719dd1d2
CassHsu <cass.hsu@gmail.com>
2022-01-09 09:34:20 +0800

    Create 2124-01.py

---
+++ 2124/2124-01.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        last_a = 0
+        first_b = len(s)
+        
+        for i, c in enumerate(s):
+            if c == "a":
+                last_a = i
+            elif first_b == len(s) and c == "b":
+                first_b = i
+        
+        return last_a <= first_b
