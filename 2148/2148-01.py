bdfca08b75addedf9ee31fba450154a68f0fcb84
CassHsu <cass.hsu@gmail.com>
2022-01-28 06:49:52 +0800

    Update 2148-01.py

--- 2148/2148-01.py
+++ 2148/2148-01.py
@@ -2,9 +2,11 @@ class Solution:
     def countElements(self, nums: List[int]) -> int:
         count = 0
         nums.sort()
+        mx = max(nums)
+        mi = min(nums)
         
         for i in range(1, len(nums) - 1):
-            if min(nums[:i]) < nums[i] < max(nums[i+1:]):
+            if mi < nums[i] < mx:
                 count += 1
         
         return count
