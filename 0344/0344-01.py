ec552b81ad5b64175e453503d6b35ab3129ad35d
CassHsu <cass.hsu@gmail.com>
2022-04-02 10:45:32 +0800

    Two Pointers

--- 0344/0344-01.py
+++ 0344/0344-01.py
@@ -1,13 +1,8 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
+        i, j = 0, len(s) - 1
         
-        l = 0
-        r = len(s) - 1
-        
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
+        while i <= j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
