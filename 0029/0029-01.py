c5372fb826fd48efe5edf9a8e1776721221ecc7d
CassHsu <cass.hsu@gmail.com>
2022-05-31 22:38:44 +0800

    Create 0029-01.py

---
+++ 0029/0029-01.py
@@ -0,0 +1,7 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        res= dividend / divisor
+        if(res >= 2**31):
+            return (2**31) - 1
+        
+        return int(res)
