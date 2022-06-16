0d760bb9ff01d55e88982a7867d506a8eea8b1fd
CassHsu <cass.hsu@gmail.com>
2022-06-16 22:31:30 +0800

    DP - Bottom Up

---
+++ 1048/1048-01.py
@@ -0,0 +1,7 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:                
+        words.sort(key=len)
+        dp={}
+        for w in words:
+            dp[w] = max(dp.get(w[:i] + w[i + 1:], 0) + 1 for i in range(len(w)))
+        return max(dp.values())
