333dc3ba92b808f5174380379df7660dcea00287
CassHsu <cass.hsu@gmail.com>
2022-10-30 18:20:56 +0800

    Create 2441-01.py

---
+++ 2441/2441-01.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        m = defaultdict(int)
+        
+        for n in nums:
+            m[n] += 1
+        
+        pn = []
+        for n in nums:
+            if m[n] > 0 and m[-n] >0:
+                pn.append(n)
+        
+        return max(pn) if len(pn) > 0 else -1
