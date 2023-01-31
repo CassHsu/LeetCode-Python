44b650ef1699c93830d827a47188182b5e95d321
CassHsu <cass.hsu@gmail.com>
2023-01-31 22:19:24 +0800

    Update 1137-01.py

--- 1137/1137-01.py
+++ 1137/1137-01.py
@@ -1,19 +1,15 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n < 2:
-            return n
-        elif n == 2:
-            return 1
-        
-        dp = [-1 if i > 2 else i for i in range(n+1)]
-        dp[2] = 1
-        
-        def f(x):
+        dp = [-1 if i > 2 else i for i in range(n + 1)]
+        if len(dp) >= 3:
+            dp[2] = 1
+
+        def f(x: int):
             if dp[x] != -1:
                 return dp[x]
-            
-            dp[x] = f(x-3) + f(x-2) + f(x-1)
+
+            dp[x] = f(x - 3) + f(x - 2) + f(x - 1)
             return dp[x]
-        
+
         f(n)
         return dp[n]
