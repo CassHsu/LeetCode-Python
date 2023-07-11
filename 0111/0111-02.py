4045070069a3954fc97a3ac9c870e4a81f8f6301
CassHsu <cass.hsu@gmail.com>
2023-07-11 23:54:46 +0800

    Create 0111-02.py

---
+++ 0111/0111-02.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        
+        if not root:
+            return 0
+
+        if not root.left and not root.right:
+            return 1
+
+        elif not root.left:
+            return 1 + self.minDepth(root.right)
+        
+        elif not root.right:
+            return 1 + self.minDepth(root.left)
+        
+        else:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
